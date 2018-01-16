c440f2c04812ca64b0e58d52389818d84bbd0ce8
smdat@hueic.edu.vn <suminhdat1312>
2018-01-16 10:36:19 +0700

    finish button favourite

--- app/src/main/java/com/fstyle/androidtrainning/screen/detailmovie/DetailsMovieActivity.java
+++ app/src/main/java/com/fstyle/androidtrainning/screen/detailmovie/DetailsMovieActivity.java
@@ -1,7 +1,9 @@
 package com.fstyle.androidtrainning.screen.detailmovie;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -12,18 +14,30 @@ import com.bumptech.glide.Glide;
 import com.fstyle.androidtrainning.R;
 import com.fstyle.androidtrainning.listener.CallAPIListener;
 import com.fstyle.androidtrainning.model.Movie;
+import com.fstyle.androidtrainning.model.MovieUser;
 import com.fstyle.androidtrainning.model.Trailer;
 import com.fstyle.androidtrainning.restapi.GetMovieAsynTask;
 import com.fstyle.androidtrainning.restapi.GetTrailerMovieAsynTank;
 import com.fstyle.androidtrainning.util.Constant;
 import com.fstyle.androidtrainning.util.DateTimeUtils;
 import com.fstyle.androidtrainning.util.StringUtils;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerFragment;
 import com.google.android.youtube.player.YouTubeThumbnailLoader;
 import com.google.android.youtube.player.YouTubeThumbnailView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -32,7 +46,8 @@ import java.util.List;
 
 public class DetailsMovieActivity extends AppCompatActivity
         implements CallAPIListener, YouTubePlayer.OnInitializedListener,
-        YouTubeThumbnailView.OnInitializedListener, View.OnClickListener{
+        YouTubeThumbnailView.OnInitializedListener, View.OnClickListener,
+        GoogleApiClient.OnConnectionFailedListener{
     private ImageView mImageFavorite, mImageBigView, mImageSmallView;
     private TextView mTextTitleMovie, mTextPublishTime, mTextTimeMovie;
     private TextView mTextKindMovie, mTextRate, mTextOverview;
@@ -42,14 +57,26 @@ public class DetailsMovieActivity extends AppCompatActivity
     private YouTubePlayer.OnInitializedListener mOnPlayerInitListener;
     private RelativeLayout mRelativeLayout;
     private static final String TAG = "DetailsMovieActivity";
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private DatabaseReference mUserCloudEndPoint;
+    private FirebaseDatabase mFirebaseInstance;
+    private GoogleApiClient mGoogleApiClient;
+
+    private String urlPoster, titleMovie;
+    private Integer movieId;
+    private String mUserId;
+    private String mUserName;
+    private String mUserEmail;
+    private MovieUser mUser;
+    private String userId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
 
-        Integer movieId = getIntent().getIntExtra(Constant.EXTRA_MOVIE_ID, 0);
-
-        initViews();
+        movieId = getIntent().getIntExtra(Constant.EXTRA_MOVIE_ID, 0);
 
         new GetMovieAsynTask(DetailsMovieActivity.this).execute(
                 "https://api.themoviedb.org/3/movie/" + movieId + "?api_key="
@@ -60,16 +87,8 @@ public class DetailsMovieActivity extends AppCompatActivity
                         + "/videos?api_key=" + Constant.API_KEY
         );
 
-        mImageFavorite.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (!mImageFavorite.isSelected()) {
-                    mImageFavorite.setSelected(true);
-                } else {
-                    mImageFavorite.setSelected(false);
-                }
-            }
-        });
+        initViews();
+
     }
 
     private void initViews() {
@@ -92,6 +111,7 @@ public class DetailsMovieActivity extends AppCompatActivity
                 .replace(R.id.frame_fragment, mYouTubePlayer)
                 .commit();
     }
+
     private void fillData(Movie movie) {
         if (movie != null) {
             if (movie.getBackdropPath() != null) {
@@ -100,7 +120,7 @@ public class DetailsMovieActivity extends AppCompatActivity
                 Glide.with(this).load(urlBackdrop).into(mImageBigView);
             }
             if (movie.getPosterPath() != null) {
-                String urlPoster = StringUtils.convertPosterPathToUrlPoster(movie.getPosterPath());
+                urlPoster = StringUtils.convertPosterPathToUrlPoster(movie.getPosterPath());
                 Glide.with(this).load(urlPoster).into(mImageSmallView);
             }
             String genresCommaSeparated = StringUtils
@@ -112,6 +132,7 @@ public class DetailsMovieActivity extends AppCompatActivity
             String releaseDate = DateTimeUtils.getStrDateTimeFormatted(date,
                     Constant.DATE_FORMAT_DD_MMM_YYYY);
 
+            titleMovie = movie.getTitle();
             mTextTitleMovie.setText(movie.getTitle());
             mTextPublishTime.setText(releaseDate);
             mTextTimeMovie.setText(runTimeMovie);
@@ -128,7 +149,10 @@ public class DetailsMovieActivity extends AppCompatActivity
 
     @Override
     public void onCallAPISuccess(List mList) {
-        if (mList != null) {
+        showButtonFavourite();
+        mImageFavorite.setOnClickListener(this);
+
+        if (mList != null && mList.size() > 0) {
             mThumbnailView.setVisibility(View.VISIBLE);
             if (mList instanceof List) {
                 if (((List<Movie>) mList).get(0) instanceof Movie) {
@@ -137,11 +161,12 @@ public class DetailsMovieActivity extends AppCompatActivity
                         fillData(movie);
                     }
                 } else {
-                    List<Trailer> trailers = mList;
-                    showMovieOnGrid(trailers);
+                    if (((List<Trailer>) mList).get(0) instanceof Trailer) {
+                        List<Trailer> trailers = mList;
+                        showMovieOnGrid(trailers);
+                    }
                 }
             }
-
         } else {
             mRelativeLayout.setVisibility(View.INVISIBLE);
             mRelativeLayout.setVisibility(View.GONE);
@@ -197,15 +222,117 @@ public class DetailsMovieActivity extends AppCompatActivity
         if (mListTrailer.isEmpty()) {
             Toast.makeText(this, "No Result Found", Toast.LENGTH_SHORT).show();
         } else {
-            for (Trailer trailer : mListTrailer) {
-                if (trailer.getName().contains(Constant.OFFICIAL)) {
-                    mYoutubeKey = trailer.getKey();
-                    break;
-                } else {
-                    mYoutubeKey = mListTrailer.get(Constant.FIRST_TRAILER).getKey();
+            if (mListTrailer != null) {
+                for (Trailer trailer : mListTrailer) {
+                    if (trailer.getName().contains(Constant.OFFICIAL)) {
+                        mYoutubeKey = trailer.getKey();
+                        break;
+                    } else {
+                        mYoutubeKey = mListTrailer.get(Constant.FIRST_TRAILER).getKey();
+                    }
                 }
+                mThumbnailView.initialize(Constant.GOOGLE_API_KEY, this);
             }
-            mThumbnailView.initialize(Constant.GOOGLE_API_KEY, this);
+        }
+    }
+
+    private void createUser(String name, String email, Integer movieID,
+            String originalTitleMovie, String posterUrl) {
+        if (TextUtils.isEmpty(mUserId)) {
+            mUserId = mUserCloudEndPoint.push().getKey();
+        }
+        MovieUser user = new MovieUser();
+        user.setEmail(email);
+        user.setName(name);
+        ArrayList<HashMap<String, String>> favouriteMoVie =
+                new ArrayList<HashMap<String, String>>();
+        HashMap<String, String> movie = new HashMap<String, String>();
+        movie = getMovie(movieID.toString(), originalTitleMovie, posterUrl);
+
+        favouriteMoVie.add(movie);
+        user.setFavouriteMovies(favouriteMoVie);
+        mUserCloudEndPoint.child(mUserId).setValue(user);
+    }
+
+    private void addMovie(Integer movieID, String originalTitleMovie,
+            String posterUrl, Integer key) {
+        HashMap<String, String> movie = new HashMap<String, String>();
+        movie = getMovie(movieID.toString(), originalTitleMovie, posterUrl);
+
+        mUserCloudEndPoint.child(userId).child("favouriteMovies").child(key.toString())
+                .setValue(movie);
+    }
+
+    private  HashMap<String, String> getMovie(String id, String title, String url) {
+        HashMap<String, String> movie = new HashMap<String, String>();
+        movie.put("id", id);
+        movie.put("original_title", title);
+        movie.put("poster_url", url);
+        return movie;
+    }
+
+    private void initFirebase() {
+
+        mUserName = mFirebaseUser.getDisplayName();
+        mUserEmail = mFirebaseUser.getEmail();
+        // store app title to 'app_title' node
+
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        mFirebaseInstance.getReference(Constant.APP_NODE_FIREBASE).setValue(Constant
+                .APP_TITLE_FIREBASE);
+
+        // get reference to 'users' node
+        mUserCloudEndPoint = mFirebaseInstance.getReference(Constant.USERS_NODE_FIREBASE);
+    }
+
+    private boolean checkUser(DataSnapshot dataSnapshot) {
+        userId = mFirebaseInstance.getReference().getKey();
+        for (DataSnapshot categorySnapshot : dataSnapshot.getChildren()) {
+            mUser = categorySnapshot.getValue(MovieUser.class);
+            if (mUser.getEmail().equals(mUserEmail)) {
+                userId = categorySnapshot.getKey();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void showButtonFavourite() {
+        //show button Favourite
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+        if (mFirebaseUser == null) {
+            mImageFavorite.setVisibility(View.INVISIBLE);
+        } else {
+
+            initFirebase();
+            mUserCloudEndPoint.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                    if (checkUser(dataSnapshot)) {
+                        HashMap<String, String> movie = new HashMap<String, String>();
+                        movie = getMovie(movieId.toString(), titleMovie, urlPoster);
+
+                        Integer key = 0;
+                        for (HashMap<String, String> favouriteMovie : mUser.getFavouriteMovies()) {
+
+                            if (favouriteMovie.equals(movie)) {
+                                mImageFavorite.setSelected(true);
+                                break;
+                            }
+                            key++;
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+            mImageFavorite.setVisibility(View.VISIBLE);
         }
     }
 
@@ -217,8 +344,75 @@ public class DetailsMovieActivity extends AppCompatActivity
                 mRelativeLayout.setVisibility(View.GONE);
                 mYouTubePlayer.initialize(Constant.GOOGLE_API_KEY, this);
                 break;
+            case R.id.imageFavorite:
+                initFirebase();
+                if (!mImageFavorite.isSelected()) {
+                    // Check for already existed user
+                    mUserCloudEndPoint.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            Boolean isExistUser = false;
+                            if (checkUser(dataSnapshot)) {
+                                    int numOfMovie = mUser.getFavouriteMovies().size();
+                                    addMovie(movieId, titleMovie,
+                                            urlPoster, numOfMovie);
+                            } else {
+                                createUser(mUserName, mUserEmail, movieId, titleMovie, urlPoster);
+                                mUserCloudEndPoint.getDatabase();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+                    mImageFavorite.setSelected(true);
+                } else {
+
+                    mUserCloudEndPoint.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if (checkUser(dataSnapshot)) {
+                                HashMap<String, String> movie = new HashMap<String, String>();
+                                movie = getMovie(movieId.toString(), titleMovie, urlPoster);
+
+                                ArrayList<HashMap<String, String>> favouriteList = new ArrayList
+                                        <HashMap<String, String>>();
+                                for (HashMap<String, String> favouriteMovie : mUser
+                                        .getFavouriteMovies()) {
+                                    favouriteList.add(favouriteMovie);
+                                }
+
+                                for (HashMap<String, String> movie1 : favouriteList) {
+                                    if (movie1.equals(movie)) {
+                                        favouriteList.remove(movie1);
+                                        break;
+                                    }
+                                }
+
+                                mUserCloudEndPoint.child(userId).child("favouriteMovies")
+                                        .setValue(favouriteList);
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
+                    mImageFavorite.setSelected(false);
+                }
+
+                break;
             default:
                 break;
         }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, R.string.connection_failed + connectionResult.getErrorMessage());
     }
 }
---
+++ app/src/main/java/com/fstyle/androidtrainning/screen/signin/SignInActivity.java
@@ -0,0 +1,117 @@
+package com.fstyle.androidtrainning.screen.signin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import com.fstyle.androidtrainning.R;
+import com.fstyle.androidtrainning.screen.home.MainActivity;
+import com.fstyle.androidtrainning.util.Constant;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class SignInActivity extends AppCompatActivity
+        implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+    private static final String TAG = SignInActivity.class.getSimpleName();
+    private SignInButton mSignInButton;
+    private GoogleApiClient mGoogleApiClient;
+    private FirebaseAuth mFirebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        // Assign fields
+        mSignInButton = (SignInButton) findViewById(R.id.sign_in_button);
+
+        // Set click listeners
+        mSignInButton.setOnClickListener(this);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions
+                .DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        // Initialize FirebaseAuth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, Constant.RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == Constant.RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed
+                Log.e(TAG, String.valueOf(R.string.signin_failed));
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mFirebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(SignInActivity.this,
+                                    String.valueOf(R.string.signin_failed),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            startActivity(new Intent(SignInActivity.this,
+                                    MainActivity.class));
+                            finish();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, String.valueOf(R.string.google_service_error),
+                Toast.LENGTH_SHORT).show();
+    }
+
+}
